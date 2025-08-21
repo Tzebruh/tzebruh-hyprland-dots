aada34343a25f3d90306d792721c141178ce9566
Tzebruh <justincoffman.dev@outlook.com>
2025-08-21 15:08:10 -0400

    Add Hyprshot, bound to Print Screen

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo pacman -Syu hyprland hyprpaper waybar wofi foot nautilus pavucontrol
+sudo pacman -Syu hyprland hyprpaper hyprshot waybar wofi foot nautilus pavucontrol
 cp -a ./.config/. ~/.config/
