f68232bfbbd219b617c8e841e32b78ed6bc8660a
Tzebruh <justincoffman.dev@outlook.com>
2025-08-26 17:15:14 -0400

    Add Hyprlock (SUPER+L)

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo pacman -Syu --needed hyprland hyprpaper hyprshot waybar wofi foot nautilus dunst otf-font-awesome pavucontrol ttf-dejavu-nerd
+sudo pacman -Syu --needed hyprland hyprpaper hyprlock hyprshot waybar wofi foot nautilus dunst otf-font-awesome pavucontrol ttf-dejavu-nerd
 cp -a ./.config/. ~/.config/
