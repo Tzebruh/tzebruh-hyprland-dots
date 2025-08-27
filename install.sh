c88539bea954f587953965998399eaee8af750ec
Tzebruh <justincoffman.dev@outlook.com>
2025-08-26 20:01:48 -0400

    Make Nautilus the default XDG file manager in install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 sudo pacman -Syu --needed hyprland hyprpaper hyprlock hyprshot waybar wofi foot nautilus dunst otf-font-awesome pavucontrol ttf-dejavu-nerd
 cp -a ./.config/. ~/.config/
+xdg-mime default org.gnome.Nautilus.desktop inode/directory
