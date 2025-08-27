d59ead4228bc23a852a8df73ee3aaccf4beb9d59
Tzebruh <justincoffman.dev@outlook.com>
2025-08-26 20:38:01 -0400

    Add packages to install.sh needed for dark mode on gtk apps without gdm

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo pacman -Syu --needed hyprland hyprpaper hyprlock hyprshot waybar wofi foot nautilus dunst otf-font-awesome pavucontrol ttf-dejavu-nerd
+sudo pacman -Syu --needed hyprland hyprpaper hyprlock hyprshot waybar wofi foot nautilus dunst otf-font-awesome pavucontrol ttf-dejavu-nerd xdg-desktop-portal-gtk xdg-desktop-portal-hyprland
 cp -a ./.config/. ~/.config/
 xdg-mime default org.gnome.Nautilus.desktop inode/directory
