275b98a6647c366a12e254bce3ecb2933e93d83f
Tzebruh <justincoffman.dev@outlook.com>
2025-09-06 22:20:38 -0400

    Add hyprpolkitagent

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo pacman -Syu --needed hyprland hyprpaper hyprlock hyprshot waybar wofi foot nautilus dunst otf-font-awesome pavucontrol ttf-dejavu-nerd xdg-desktop-portal-gtk xdg-desktop-portal-hyprland gnome-themes-extra qt6ct
+sudo pacman -Syu --needed hyprland hyprpaper hyprlock hyprshot waybar wofi foot nautilus dunst otf-font-awesome pavucontrol ttf-dejavu-nerd xdg-desktop-portal-gtk xdg-desktop-portal-hyprland gnome-themes-extra qt6ct hyprpolkitagent
 cp -a ./.config/. ~/.config/
 xdg-mime default org.gnome.Nautilus.desktop inode/directory
