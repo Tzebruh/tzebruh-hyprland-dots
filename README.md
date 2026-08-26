14851188c9c598aa5ba5ff67b81115e972ba798d
Tzebruh <justincoffman.dev@outlook.com>
2026-08-26 00:52:18 -0400

    Experimental Debian support

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ These are my personal Hyprland configuration files. I'm an absolute beginner to 
 # Notes
 - To set dark theme on QT apps, open `qt6ct` and select your dark theme. The environment variable for QT themes is just set to qt6ct so manual configuration is needed for that.
 - Since Hyprland updated to use lua config, Waybar's `hyprland/workspaces` module has been broken. To fix it, use an AUR helper like `yay` to instead install `waybar-git` for now. I have no idea when it will be fixed in the pacman version!
+- Debian support is super experimental yada yada basically it sucks
---
+++ install-debian.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Setup backports repo
+sudo cp trixie-backports.list /etc/apt/sources.list.d/trixie-backports.list
+sudo apt update
+
+# Install backported packages
+sudo apt install -t trixie-backports hyprland hyprpaper hyprlock waybar xdg-desktop-portal-hyprland pipewire pipewire-pulse hyprland-guiutils
+
+# Install normal packages
+sudo apt install wofi foot nautilus dunst fonts-font-awesome pavucontrol xdg-desktop-portal-gtk gnome-themes-extra qt6ct polkit-kde-agent-1 brightnessctl wl-clipboard
+
+# Missing from Arch: hyprshot, ttf-dejavu-nerd
+
+# Configure
+cp -a ./.config/. ~/.config/
+cp foot-debian.ini ~/.config/foot/foot.ini
+sudo ln -s /usr/lib/x86_64-linux-gnu/libexec/polkit-kde-authentication-agent-1 /usr/lib/polkit-kde-authentication-agent-1
+xdg-mime default org.gnome.Nautilus.desktop inode/directory
+
+# Manual install of DejaVuSansM Nerd Font
+mkdir -p ~/.local/share/fonts/DejaVuSansMono
+cd ~/.local/share/fonts/DejaVuSansMono
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.5.1/DejaVuSansMono.zip
+unzip DejaVuSansMono.zip
+rm DejaVuSansMono.zip
+fc-cache -fv
