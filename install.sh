40f86316370726d081bd5d78e76d48b1409744ba
Tzebruh <justincoffman.dev@outlook.com>
2026-08-25 22:50:14 -0400

    Add wl-copy to install script (idk if it's a dependency of something or not but I'm putting it in just in case)

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo pacman -Syu --needed hyprland hyprpaper hyprlock hyprshot waybar wofi foot nautilus dunst otf-font-awesome pavucontrol ttf-dejavu-nerd xdg-desktop-portal-gtk xdg-desktop-portal-hyprland gnome-themes-extra qt6ct polkit-kde-agent pipewire pipewire-pulse brightnessctl
+sudo pacman -Syu --needed hyprland hyprpaper hyprlock hyprshot waybar wofi foot nautilus dunst otf-font-awesome pavucontrol ttf-dejavu-nerd xdg-desktop-portal-gtk xdg-desktop-portal-hyprland gnome-themes-extra qt6ct polkit-kde-agent pipewire pipewire-pulse brightnessctl wl-copy
 cp -a ./.config/. ~/.config/
 xdg-mime default org.gnome.Nautilus.desktop inode/directory
