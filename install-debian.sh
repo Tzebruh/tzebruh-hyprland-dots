ca009051bcb89031c7b5e2906241a63fb1e89b0c
Tzebruh <justincoffman.dev@outlook.com>
2026-09-01 15:04:52 -0400

    Add notification for volume changes (and organize the tze-* binaries)

--- install-debian.sh
+++ install-debian.sh
@@ -14,8 +14,7 @@ sudo apt install wofi foot nautilus dunst fonts-font-awesome pavucontrol xdg-des
 
 # Configure
 cp -a ./.config/. ~/.config/
-cp ./tze-power-menu ~/.local/bin/tze-power-menu
-cp ./tze-battery-status ~/.local/bin/tze-battery-status
+cp -a ./.local/bin/. ~/.local/bin/
 cp ./foot-debian.ini ~/.config/foot/foot.ini
 sudo ln -s /usr/lib/x86_64-linux-gnu/libexec/polkit-kde-authentication-agent-1 /usr/lib/polkit-kde-authentication-agent-1
 xdg-mime default org.gnome.Nautilus.desktop inode/directory
