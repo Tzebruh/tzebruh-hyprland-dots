8f9b621eea20a9c37392ac681fc2b9616e73181d
Tzebruh <justincoffman.dev@outlook.com>
2026-09-08 21:00:50 -0400

    Add Gwenview and VLC to the install script

--- install-debian.sh
+++ install-debian.sh
@@ -8,7 +8,7 @@ sudo apt update
 sudo apt install -t trixie-backports hyprland hyprpaper hyprlock waybar xdg-desktop-portal-hyprland pipewire pipewire-pulse hyprland-guiutils hyprshutdown
 
 # Install normal packages
-sudo apt install wofi foot dolphin dunst fonts-font-awesome pavucontrol xdg-desktop-portal-gtk gnome-themes-extra qt6ct polkit-kde-agent-1 brightnessctl wl-clipboard jq grim slurp libnotify-bin hyprpicker power-profiles-daemon
+sudo apt install wofi foot dolphin dunst fonts-font-awesome pavucontrol xdg-desktop-portal-gtk gnome-themes-extra qt6ct polkit-kde-agent-1 brightnessctl wl-clipboard jq grim slurp libnotify-bin hyprpicker power-profiles-daemon gwenview vlc
 
 # Missing from Arch: hyprshot, ttf-dejavu-nerd
 
@@ -18,6 +18,12 @@ cp -a ./.local/bin/. ~/.local/bin/
 cp ./foot-debian.ini ~/.config/foot/foot.ini
 sudo ln -s /usr/lib/x86_64-linux-gnu/libexec/polkit-kde-authentication-agent-1 /usr/lib/polkit-kde-authentication-agent-1
 xdg-mime default org.gnome.dolphin.desktop inode/directory
+for mime in image/jpeg image/png image/gif image/webp image/svg+xml image/avif; do
+	xdg-mime default org.kde.gwenview.desktop "$mime"
+done
+for mime in video/mp4 video/x-matroska video/webm video/quicktime video/x-msvideo; do
+	xdg-mime default vlc.desktop "$mime"
+done
 
 # Manual install of hyprshot
 sudo wget -O /usr/bin/hyprshot https://raw.githubusercontent.com/Gustash/Hyprshot/refs/heads/main/hyprshot
--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-sudo pacman -Syu --needed hyprland hyprpaper hyprlock hyprshot waybar wofi foot dolphin dunst otf-font-awesome pavucontrol ttf-dejavu-nerd xdg-desktop-portal-gtk xdg-desktop-portal-hyprland gnome-themes-extra qt6ct polkit-kde-agent pipewire pipewire-pulse brightnessctl wl-copy hyprshutdown hyprpicker power-profiles-daemon
+sudo pacman -Syu --needed hyprland hyprpaper hyprlock hyprshot waybar wofi foot dolphin dunst otf-font-awesome pavucontrol ttf-dejavu-nerd xdg-desktop-portal-gtk xdg-desktop-portal-hyprland gnome-themes-extra qt6ct polkit-kde-agent pipewire pipewire-pulse brightnessctl wl-copy hyprshutdown hyprpicker power-profiles-daemon gwenview vlc
 cp -a ./.config/. ~/.config/
 cp -a ./.local/bin/. ~/.local/bin/
 xdg-mime default org.kde.dolphin.desktop inode/directory
+for mime in image/jpeg image/png image/gif image/webp image/svg+xml image/avif; do
+	xdg-mime default org.kde.gwenview.desktop "$mime"
+done
+for mime in video/mp4 video/x-matroska video/webm video/quicktime video/x-msvideo; do
+	xdg-mime default vlc.desktop "$mime"
+done
