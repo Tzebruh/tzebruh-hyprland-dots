69f2e4f8bc9f05b1970712a248422e542807e46e
Tzebruh <justincoffman.dev@outlook.com>
2026-09-12 10:08:21 -0400

    Use the correct arch package name for wl-clipboard

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo pacman -Syu --needed hyprland hyprpaper hyprlock hyprshot waybar wofi foot nautilus dunst otf-font-awesome pavucontrol ttf-dejavu-nerd xdg-desktop-portal-gtk xdg-desktop-portal-hyprland gnome-themes-extra qt6ct polkit-kde-agent pipewire pipewire-pulse brightnessctl wl-copy hyprshutdown hyprpicker power-profiles-daemon gwenview vlc jq
+sudo pacman -Syu --needed hyprland hyprpaper hyprlock hyprshot waybar wofi foot nautilus dunst otf-font-awesome pavucontrol ttf-dejavu-nerd xdg-desktop-portal-gtk xdg-desktop-portal-hyprland gnome-themes-extra qt6ct polkit-kde-agent pipewire pipewire-pulse brightnessctl wl-clipboard hyprshutdown hyprpicker power-profiles-daemon gwenview vlc jq
 cp -a ./.config/. ~/.config/
 cp -a ./.local/bin/. ~/.local/bin/
 xdg-mime default org.gnome.Nautilus.desktop inode/directory
